6826c5edc0284a40b5f9aca4e53018d28f3a3fa7
Rex <rexyyong@gmail.com>
2023-05-29 00:03:15 +0800

    Fixed bug where login here button redirects wrong
    Tried to do static files to display image but it didn't work

--- gfg/settings.py
+++ gfg/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
