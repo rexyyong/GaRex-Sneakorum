28bfbb42c8db5a7132e8baad0678dc3a1a082466
Rex <rexyyong@gmail.com>
2023-05-31 11:55:00 +0800

    Fixed logo issue, downlaoded deployment packages
    Fixed the static file issue, now logo loads in the websites
    
    downloaded a few packages according to the render 40 min video

--- gfg/settings.py
+++ gfg/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#static directory path
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,7 +79,6 @@ WSGI_APPLICATION = 'gfg.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -116,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
