0f4e2ceb88bb109e1bc9636edb0bdcf17ad244c1
Rex <rexyyong@gmail.com>
2023-06-19 16:47:41 +0800

    Creating Forum frontend on reac

---
+++ forum-react-app/src/pages/ForumHome.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+//MUI stuff
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+
+
+return (
+    <div style={{ marginTop: 100 }}>
+
+
+        
+    </div>
+
+)
