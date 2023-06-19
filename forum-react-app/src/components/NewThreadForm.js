4c45158132022d312b8ec321b88a3ffb00e33c22
Rex <rexyyong@gmail.com>
2023-06-19 23:17:43 +0800

    Pop up dialog box for New Thread button
    when submit button is pressed for the dialog box
    title and content are logged to console log

--- forum-react-app/src/components/NewThreadForm.js
+++ forum-react-app/src/components/NewThreadForm.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState, useEffect, useContext } from 'react';
 
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { createTheme } from '@mui/material/styles';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import AddBoxIcon from '@mui/icons-material/AddBox';
+
+//related to dialog box
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 // new thread button theme
 const buttonTheme = createTheme({
@@ -16,19 +25,93 @@ const buttonTheme = createTheme({
 });
 
 const NewThreadForm = () => {
+
+    //for dialog box form
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    //isOpen is the state, setIsOpen is the function to change the state, for popping up dialog box
+    const [open, setIsOpen] = useState(false);
+
+    //to open
+    const handleOpen = () => {
+        setIsOpen(true);
+    };
+
+    //to close
+    const handleClose = () => {
+        setIsOpen(false);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Perform any desired actions with the submitted data
+        console.log('Title:', title);
+        console.log('Content:', content);
+
+        //clear the form
+        setTitle('');
+        setContent('');
+
+        // Close the dialog
+        handleClose();
+    };
+
+
+
     return (
         <div>
             <Grid container justifyContent="flex-end">
                 <Button theme={buttonTheme}
-                    style={{ maxWidth: '300px', maxHeight: '40px', minWidth: '150px', minHeight: '36px'  }}
+                    style={{ maxWidth: '300px', maxHeight: '40px', minWidth: '150px', minHeight: '36px' }}
                     endIcon={<AddBoxIcon />}
                     variant="contained"
+                    onClick={handleOpen}
                 >
                     New Thread
                 </Button>
 
             </Grid>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Create New Thread</DialogTitle>
+                <DialogContent>
+                    <form onSubmit={handleSubmit} id="NewThreadForm">
+                        <TextField
+                            label="Title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            fullWidth
+                            required
+                        />
+                        <TextField
+                            label="Content"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            fullWidth
+                            multiline
+                            rows={4}
+                            margin="normal"
+                            required
+                        />
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button
+                                type="submit"
+                                form="NewThreadForm"
+                                variant="contained"
+                                color="primary"
+                                endIcon={<AddBoxIcon />} >
+                                Create
+                            </Button>
+                        </DialogActions>
+                    </form>
+                </DialogContent>
+
+            </Dialog>
         </div>
+
+
     )
 }
 
