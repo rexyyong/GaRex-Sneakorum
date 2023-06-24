1ec00935a13b6668ddf3b249c8a30318a34c57de
staircase <51813599+lordgareth10@users.noreply.github.com>
2023-06-24 23:07:40 +0800

    fixed routing issue for urls

--- gfg/urls.py
+++ gfg/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-    #path('forum', include('forum.urls')),
+    # path('', include('forum_api.urls')),
     path('forum_api/', include('forum_api.urls')),
-]
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
