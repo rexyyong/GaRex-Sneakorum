16342eca58f32690148619170018c4f39e705ad5
staircase <51813599+lordgareth10@users.noreply.github.com>
2023-07-18 17:34:05 +0800

    chagned url of fetchthread

--- forum_api/views.py
+++ forum_api/views.py
@@ -4,6 +4,7 @@ from .serializers import ThreadSerializer, CommentSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 import json
 from rest_framework import status
 
@@ -26,7 +27,7 @@ def getThread(request, thread_id):
         thread = Thread.objects.get(pk=thread_id)
     except thread.DoesNotExist:
         content = {"The Thread does not exist."}
-        return Response(content)
+        return JsonResponse(content)
     serializer = ThreadSerializer(thread, many=False)
 
     return Response(serializer.data)
