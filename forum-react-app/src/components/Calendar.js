04f4146ea4b6dd52ceca3ab81a16546c04ea0232
Rex <rexyyong@gmail.com>
2023-07-24 10:57:58 +0800

    Link now works

--- forum-react-app/src/components/Calendar.js
+++ forum-react-app/src/components/Calendar.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { Container, Grid, Box, Typography } from '@mui/material';
 import ItemData from './ItemData'; // Create an itemData.js file with sample data
 
-const ItemBox = ({ image, description, price, release_date}) => (
+const ItemBox = ({ image, description, price, release_date, link }) => (
     <Box sx={{ textAlign: 'center' }}>
-        <img src={image} alt="Item" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
-        <Typography variant="body1" mt={2}>
-            {description}
-        </Typography>
-        <Typography variant="body1">
-            {price}
-        </Typography>
-        <Typography variant="body1">
-            {release_date}
-        </Typography>
+        <a href={link} target="_blank" rel="noopener noreferrer" >
+            <img src={image} alt="Item" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+            <Typography variant="body1" mt={2}>
+                {description}
+            </Typography>
+            <Typography variant="body1">
+                {price}
+            </Typography>
+            <Typography variant="body1">
+                {release_date}
+            </Typography>
+        </a>
     </Box>
 );
 
@@ -23,7 +25,7 @@ const Calendar = () => {
             <Grid container spacing={2}>
                 {ItemData.map((item, index) => (
                     <Grid item xs={12} sm={6} md={4} key={index}>
-                        <ItemBox image={item.image} description={item.description} price={item.price} release_date={item.release_date}/>
+                        <ItemBox image={item.image} description={item.description} price={item.price} release_date={item.release_date} link={item.link} />
                     </Grid>
                 ))}
             </Grid>
