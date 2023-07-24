458e7efb6f14b5dfbff00138bd6d52f3f8839161
staircase <51813599+lordgareth10@users.noreply.github.com>
2023-07-24 12:43:58 +0800

    trying to include user into new threads

--- forum_api/urls.py
+++ forum_api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('threads/<int:thread_id>/comments', views.getComments, name='comments'),
     path('search/<str:query>', views.searchThread, name='search'),
     path('forum_api/profile/<str:username>/', views.profile, name='profile'),
+    path('forum_api/getUser/<str:username>/', views.get_user_by_username, name='getuser'),
 ]
