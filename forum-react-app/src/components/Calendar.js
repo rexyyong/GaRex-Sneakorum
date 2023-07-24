4b75ae36c71e34f696182ae3139f31f943905942
Rex <rexyyong@gmail.com>
2023-07-24 10:49:05 +0800

    price and release date keyed in

--- forum-react-app/src/components/Calendar.js
+++ forum-react-app/src/components/Calendar.js
@@ -2,27 +2,33 @@ import React from 'react';
 import { Container, Grid, Box, Typography } from '@mui/material';
 import ItemData from './ItemData'; // Create an itemData.js file with sample data
 
-const ItemBox = ({ image, description }) => (
-  <Box sx={{ textAlign: 'center' }}>
-    <img src={image} alt="Item" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
-    <Typography variant="body1" mt={2}>
-      {description}
-    </Typography>
-  </Box>
+const ItemBox = ({ image, description, price, release_date}) => (
+    <Box sx={{ textAlign: 'center' }}>
+        <img src={image} alt="Item" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+        <Typography variant="body1" mt={2}>
+            {description}
+        </Typography>
+        <Typography variant="body1">
+            {price}
+        </Typography>
+        <Typography variant="body1">
+            {release_date}
+        </Typography>
+    </Box>
 );
 
 const Calendar = () => {
-  return (
-    <Container maxWidth="md">
-      <Grid container spacing={2}>
-        {ItemData.map((item, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <ItemBox image={item.image} description={item.description} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container maxWidth="md">
+            <Grid container spacing={2}>
+                {ItemData.map((item, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <ItemBox image={item.image} description={item.description} price={item.price} release_date={item.release_date}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
 };
 
 export default Calendar;
--- forum-react-app/src/components/ItemData.js
+++ forum-react-app/src/components/ItemData.js
@@ -2,34 +2,50 @@ const ItemData = [
     {
         image: 'https://drive.google.com/uc?export=view&id=1lIDeF1v0uhUtRf7XB48aEdZ2yhwRsNfx',
         description: "Older Kids' Air Jordan 6 Low, Fierce Pink",
+        price: 'S$175', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1BRB31tFl6Ae-LC6DOXjjs8Zv7j54W-4m',
-        description: 'Description of Item 2',
+        description: 'Dunk Low, Sequoia',
+        price: 'S$189', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1LkvG9aj10RJZRhKMlO9NALn872jnt-vE',
-        description: 'Description of Item 2',
+        description: "Women's Air Jordan 1 Low OG, NC to Chi",
+        price: 'S$229', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1A0pBqhtur8to-x73NeGn8DAYZallZ1jC',
-        description: 'Description of Item 2',
+        description: 'Shox MR4 x Martine Rose, Safety Orange',
+        price: 'S$285', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1kwMgEnCzsMk-lkEPSUK3OvZiQ1QywvLt',
-        description: 'Description of Item 2',
+        description: "Women's Air Max 1, Rugged Orange and Sail",
+        price: 'S$249', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1dx4fluyKsq9GlXAPXdujNP81YtHjsjRy',
-        description: 'Description of Item 2',
+        description: 'Air Jordan 1 Low, Black Toe',
+        price: 'S$229', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1OmJ20u5GyXI_7oPGCyn7Y1pkBZIpg5Au',
-        description: 'Description of Item 2',
+        description: 'Air Jordan 3, Orewood Brown',
+        price: 'S$299', 
+        release_date: 'Release: 24 July 2023',
     },
     {
         image: 'https://drive.google.com/uc?export=view&id=1Yb8Nlf29sUV_pbr8MJU8z7-0V23yybdD',
-        description: 'Description of Item 2',
+        description: 'Air Jordan 7, Chambray',
+        price: 'S$285', 
+        release_date: 'Release: 24 July 2023',
     },
     // Add more items as needed
 ];
