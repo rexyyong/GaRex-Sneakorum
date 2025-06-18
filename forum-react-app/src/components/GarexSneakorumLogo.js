68400293f890d4607b4bcad89f25c7777e14b197
Rex <76645229+rexyyong@users.noreply.github.com>
2025-06-18 21:47:04 +0800

    Update GarexSneakorumLogo.js

--- forum-react-app/src/components/GarexSneakorumLogo.js
+++ forum-react-app/src/components/GarexSneakorumLogo.js
@@ -5,7 +5,7 @@ function GarexSneakorumLogo() {
     return (
         <div>
             <img
-                src="https://lh3.googleusercontent.com/d/1Un2cKpPeRQyxwQ7Eat1oylryjx73r-YB"
+                src="https://drive.google.com/thumbnail?id=1Un2cKpPeRQyxwQ7Eat1oylryjx73r-YB"
                 height={250}
                 alt="Garex Sneakorum Logo"
                 className="center"
